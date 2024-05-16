ca5ad0a8200e9941b496179e53457a022bbd13f4
princelokesh16 <lokeshthangaraj162002@gmail.com>
2024-05-16 11:29:29 +0530

    ai

---
+++ src/locales/default/welcome.ts
@@ -0,0 +1,50 @@
+export default {
+  button: {
+    import: '导入配置',
+    market: '逛逛市场',
+    start: 'Lets Chat',
+  },
+  guide: {
+    agents: {
+      replaceBtn: '换一批',
+      title: '新增助理推荐：',
+    },
+    defaultMessage:
+      'fgd`+` 创建自定义助手',
+    qa: {
+      q01: 'LobeHub 是什么？',
+      q02: 'LobeChat 是什么？',
+      q03: 'LobeChat 是否有社区支持？',
+      q04: 'LobeChat 支持哪些功能？',
+      q05: 'LobeChat 如何部署和使用？',
+      q06: 'LobeChat 的定价是如何的？',
+      q07: 'LobeChat 是否免费?',
+      q08: '是否有云端服务版？',
+      q09: '是否支持本地语言模型?',
+      q10: '是否支持图像识别和生成?',
+      q11: '是否支持语音合成和语音识别？',
+      q12: '是否支持插件系统？',
+      q13: '是否有自己的市场来获取 GPTs？',
+      q14: '是否支持多种 AI 服务提供商？',
+      q15: '我在使用时遇到问题应该怎么办？',
+    },
+    questions: {
+      moreBtn: '了解更多',
+      title: '大家都在问：',
+    },
+    welcome: {
+      afternoon: '下午好',
+      morning: '早上好',
+      night: '晚上好',
+      noon: '中午好',
+    },
+  },
+  header: '欢迎使用',
+  pickAgent: '或从下列助手模板选择',
+  skip: '跳过创建',
+  slogan: {
+    desc1: 'gjvjgv,',
+    desc2: '创建你的第一个助手，让我们开始吧~',
+    title: '给自己一个更聪明的大脑',
+  },
+};
