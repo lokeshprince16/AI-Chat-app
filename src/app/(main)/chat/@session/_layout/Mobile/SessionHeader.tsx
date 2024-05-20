f15cba299d8a60045c95f469933775ccf0c21be2
princelokesh16 <lokeshthangaraj162002@gmail.com>
2024-05-20 09:47:56 +0530

    ai

--- src/app/(main)/chat/@session/_layout/Mobile/SessionHeader.tsx
+++ src/app/(main)/chat/@session/_layout/Mobile/SessionHeader.tsx
@@ -23,7 +23,7 @@ const Header = memo(() => {
       left={
         <Flexbox align={'center'} gap={8} horizontal style={{ marginLeft: 8 }}>
           <UserAvatar onClick={() => router.push('/me')} size={32} />
-          <Logo type={'text'} />
+          {/* <Logo type={'text'} /> */}
           {enableWebrtc && <SyncStatusInspector placement={'bottom'} />}
         </Flexbox>
       }
